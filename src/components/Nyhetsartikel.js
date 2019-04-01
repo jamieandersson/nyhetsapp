edb1a86df0051d4c50a038a5d906868dd8bff3d4
Jamie Andersson <jamie98andersson@gmail.com>
2019-04-01 11:04:48 +0200

    lagt till komponenter

---
+++ src/components/Nyhetsartikel.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Nyhetsartikel () {
+   return (
+      <article>
+        <img src="https://source.unsplash.com/random/400x250/?person" />
+        <h2>Testnyhet</h2>
+        <p>
+          Beskrvining av nyhet
+        </p>
+        <a>Läs mer...</a>
+      </article>  
+   );
+}
+
+export default Nyhetsartikel;
